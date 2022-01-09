2d5247f401b10c78fd70706cc35093cc12de5090
Lady Gaga <jessiezheqi.li@gmail.com>
2022-01-08 17:45:19 -0800

    Add db files

---
+++ db/schema.sql
@@ -0,0 +1,26 @@
+DROP DATABASE IF EXISTS employee_tracker_db;
+CREATE DATABASE employee_tracker_db;
+
+USE employee_tracker_db;
+
+CREATE TABLE department (
+    id INTEGER AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(30) NOT NULL
+);
+
+CREATE TABLE roleType (
+    id INTEGER AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(30) NOT NULL,
+    salary DECIMAL,
+    department_id INTEGER,
+    FOREIGN KEY (department_id) REFERENCES department(id) ON DELETE SET NULL
+);
+
+CREATE TABLE employee (
+    id INTEGER AUTO_INCREMENT PRIMARY KEY,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    role_id INTEGER,
+    manager_id INTEGER,
+    FOREIGN KEY (role_id) REFERENCES roleType(id) ON DELETE SET NULL
+);
---
+++ db/seeds.sql
@@ -0,0 +1,33 @@
+-- DEPARTMENT --
+INSERT INTO department (name)
+VALUES 
+("Sales"),
+("Engineering"),
+("Finance"),
+("Legal");
+
+-- ROLE --
+INSERT INTO roleType (title, salary, department_id)
+VALUES 
+("Sales Lead", 100000, 1),
+("Salesperson", 80000, 1),
+("Lead Engineer", 150000, 2),
+("Software Engineer", 120000, 2),
+("Accountant", 125000, 3),
+("Legal Team Lead", 250000, 4),
+("Lawyer", 190000, 4);
+
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES 
+('John', 'Doe', 1, 3),
+('Mike', 'Chan', 2, 1),
+('Ashley', 'Rodriguez', 3, null),
+('Kevin', 'Tupik', 4, 3),
+('Made', 'Up', 4, 3),
+('Malia', 'Brown', 5, null),
+('Sarah', 'Lourd', 6, null),
+('Tom', 'Allen', 7, 7);
+
+SELECT * FROM department;
+SELECT * FROM roleType;
+SELECT * FROM employee;
